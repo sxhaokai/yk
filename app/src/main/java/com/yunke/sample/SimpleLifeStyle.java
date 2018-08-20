15a9362076042aea2152c5b84dab19acb3fe0321
haokai <291251291@qq.com>
2018-08-20 15:33:58 +0800

    modify ActivityLifeStyle method

--- app/src/main/java/com/yunke/sample/SimpleLifeStyle.java
+++ app/src/main/java/com/yunke/sample/SimpleLifeStyle.java
@@ -1,32 +1,47 @@
-package com.yunke.view;
+package com.yunke.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.provider.Settings;
+
+import com.yunke.view.ActivityLifestyle;
 
 /**
  * Created by haokai on 2018/8/20.
  */
 
-public abstract class ActivityLifestyle {
+public class SimpleLifeStyle extends ActivityLifestyle{
+    @Override
     public void onCreateBefore(Activity activity, Bundle savedInstanceState) {
+        super.onCreateBefore(activity, savedInstanceState);
     }
 
+    @Override
     public void onResumeBefore(Activity activity) {
+        super.onResumeBefore(activity);
     }
 
+    @Override
     public void onResumeAfter(Activity activity) {
+        super.onResumeAfter(activity);
     }
 
+    @Override
     public void onStartAfter(Activity activity) {
+        super.onStartAfter(activity);
     }
 
+    @Override
     public void onPauseBefore(Activity activity) {
+        super.onPauseBefore(activity);
     }
 
+    @Override
     public void onPauseAfter(Activity activity) {
+        super.onPauseAfter(activity);
     }
 
+    @Override
     public void onDestroyBefore(Activity activity) {
+        super.onDestroyBefore(activity);
     }
 }
