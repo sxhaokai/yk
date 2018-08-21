e82115ab3ae4520a88d4166f95b9ee0a39c58cec
haokai <291251291@qq.com>
2018-08-21 11:17:20 +0800

    优化

--- yunkev/src/main/java/com/yunke/view/YunkeCard.java
+++ yunkev/src/main/java/com/yunke/view/YunkeCard.java
@@ -11,7 +11,7 @@ import com.yunke.entity.YunkeEntity;
 
 
 /**
- * Created by ChinaNetPro on 2016/11/2.
+ * Created by haokai on 2016/11/2.
  */
 
 public abstract class YunkeCard<T extends YunkeEntity> extends RecyclerView.ViewHolder implements View.OnClickListener{
@@ -38,14 +38,36 @@ public abstract class YunkeCard<T extends YunkeEntity> extends RecyclerView.View
 
     protected void bind(T t) {
         this.mEntity = t;
-    }
-
-    public T getEntity(){
-        return mEntity;
+        if (null == this.mEntity) {
+            return;
+        }
+        bindData(this.mEntity);
     }
 
     @Override
     public void onClick(View v) {
+        if (v == itemView) {
+            if (null == mEntity) {
+                return;
+            }
+            onItemClick(v ,mEntity);
+        }
+    }
 
+    /**
+     * recyclerView 条目点击 onClick()中已经做了非空判断
+     * @param v
+     * @param mEntity
+     */
+    protected abstract void onItemClick(View v, T mEntity);
+
+    /**
+     * 绑定数据 bind(T) 里面已经做了null 判断 这个里面不用做
+     * @param t
+     */
+    protected abstract void bindData(T t);
+
+    public T getEntity(){
+        return mEntity;
     }
 }
